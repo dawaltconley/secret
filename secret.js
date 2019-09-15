0d9483fc1363dc1d2b6ee149b2633015f509104e
Dylan Awalt-Conley <web@dylan.ac>
2019-09-15 14:48:42 -0400

    initial commit, basic module that can retrieve password using security child process

---
+++ secret.js
@@ -0,0 +1,79 @@
+const spawn = require('child_process').spawn
+const readline = require('readline')
+const yn = require('./yn')
+
+const parseSecurityOutput = output => {
+    const obj = {
+        keychain: /(?<=keychain: ).*/,
+        account: /(?<="acct"<blob>\=).*/,
+        service: /(?<="svce"<blob>\=).*/,
+        domain: /(?<="srvr"<blob>\=).*/,
+        path: /(?<="path"<blob>\=).*/,
+        protocol: /(?<="ptcl"<blob>\=).*/,
+        password: /(?<=password: ).*/
+    }
+    for (let k in obj) {
+        let v = output.match(obj[k])
+        if (!v) {
+            obj[k] = undefined
+            continue
+        }
+        v = v[0]
+        if (v === '<NULL>') {
+            obj[k] = null
+        } else if (/0x([0-9a-fA-F]+)/.test(v)) {
+            let hexVal = v.match(/(?<=0x)[0-9a-fA-F]+/)[0]
+            obj[k] = Buffer.from(hexVal, 'hex').toString()
+        } else {
+            obj[k] = v.match(/"(.*)"/)[1]
+        }
+    }
+    if (obj.protocol === 'htps') obj.protocol = 'https'
+    return obj
+}
+
+const getKey = (label, type, ex, attempt = 0) => new Promise((resolve, reject) => {
+    let result = ''
+    const keyTypes = ['generic', 'internet']
+    if (type === undefined) {
+        type = keyTypes[attempt]
+    } else if (!keyTypes.includes(type)) {
+        reject('Invalid key type: options are ' + keyTypes.join(', '))
+    }
+    type = type === undefined ? keyTypes[attempt] : type
+    const security = spawn(ex, [ 'find-'+type+'-password', '-l', label, '-g' ])
+    security.on('error', e => reject(e)) // failed to spawn child process
+    security.stdout.on('data', d => result += d.toString())
+    security.stderr.on('data', d => result += d.toString())
+    security.on('close', code => {
+        if (code === 0) {
+            resolve({ 
+                label: label,
+                type: type,
+                ...parseSecurityOutput(result)
+            })
+        } else {
+            // password not found, retry
+            attempt++
+            if (attempt < keyTypes.length) {
+                resolve(getKey(label, undefined, ex, attempt))
+            } else {
+                reject('Password not found')
+            }
+        }
+    })
+})
+
+class Secret {
+    constructor(label, type) {
+        this.label = label,
+        this.type = type,
+        this.executablePath = '/usr/bin/security'
+    }
+
+    get() {
+        return getKey(this.label, this.type, this.executablePath)
+    }
+}
+
+module.exports = Secret
